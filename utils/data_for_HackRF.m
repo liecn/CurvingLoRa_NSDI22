3554e1cd550ed60dfdb5e25adeb6c68b317ee317
liecn <lichenni@msu.edu>
2022-11-27 16:37:31 -0500

    wrap up

--- utils/data_for_HackRF.m
+++
@@ -1,9 +0,0 @@
-function [output] = data_for_HackRF(Payload,sf,coefficient_matrix,bw,fs,dutycycle,filename)
-output = packet_generation_curving(Payload,sf,coefficient_matrix,bw,fs);
-output = reshape(output,1,[]);
-idle_count = size(output,2)*(1/dutycycle-1);
-idle = zeros(1,idle_count);
-output = [output,idle];
-fileID = fopen(filename,'w');
-fwrite(fileID, output,'float');
-fclose(fileID); 
